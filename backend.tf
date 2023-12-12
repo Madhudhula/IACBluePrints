32a6ac0fe6f532c0a61d9be2071fc551944deb0b
Madhudhula <91835288+Madhudhula@users.noreply.github.com>
2023-12-12 13:13:21 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 #terraform {
 #  backend "s3" {
-#    bucket = "devops-cicd-terraform-eksdeploy"
+#    bucket = "devops-cicd-terraform-eksd"
 #    region = "ap-south-1"
 #    key = "jenkins-server/terraform.tfstate"
 #  }
